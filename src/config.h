0ec63f1430a148f9e5dfb56d8a50643ab67d001b
whaac1289-debug <whaac1289@gmail.com>
2026-02-04 12:34:45 +0500

    Enhance DLP policy and event logging

--- src/config.h
+++ src/config.h
@@ -7,6 +7,11 @@ extern std::string g_server_url;
 extern std::vector<std::string> g_extension_filter;
 extern size_t g_size_threshold;
 extern std::vector<std::string> g_usb_allow_serials;
+extern std::vector<std::string> g_content_keywords;
+extern size_t g_max_scan_bytes;
+extern size_t g_hash_max_bytes;
+extern bool g_block_on_match;
+extern bool g_alert_on_removable;
 
 extern std::atomic<bool> g_running;
 
--- src/sqlite_store.h
+++ src/sqlite_store.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <string>
 
+struct FileEvent;
+
 bool sqlite_init(const char *path);
 void sqlite_insert_event(const std::string &ev);
 void sqlite_insert_log(const char *ts, const char *level, const char *msg);
+void sqlite_insert_file_event(const struct FileEvent &ev);
+void sqlite_insert_device_event(const std::string &drive, const std::string &serial, bool allowed);
